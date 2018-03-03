34e45cf7e037b634853fbbd25c1313be7b2e88d5
NoName115 <robert.kolcun@gmail.com>
2018-03-03 21:10:52 +0100

    [feat] sorting for single folder added

--- src/dataset_processing/image_sorter.py
+++ src/dataset_processing/image_sorter.py
@@ -6,54 +6,62 @@ import glob
 import shutil
 
 
-print(glob.glob("/home/adam/*.txt"))
-
-'''
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-args = vars(ap.parse_args())
-'''
-
-folders_to_load = glob.glob(
-    '../dataset/move_images_2/*'
+ap.add_argument(
+    "--images",
+    required=True,
+	help="path to unsorted images"
 )
-folder_long_scene = '../dataset/long_scene/'
-folder_long_weapon = '../dataset/long_weapon/'
-folder_short_scene = '../dataset/short_scene/'
-folder_short_weapon = '../dataset/short_weapon/'
-folder_other = '../dataset/other/'
+args = vars(ap.parse_args())
 
-for folder in folders_to_load:
-    images_to_load = glob.glob(folder + '/*.jpg')
-    print(folder)
+'''
+folders_to_load = glob.glob(
+    args['images'] + '*'
+)
+'''
+folder_long_scene = 'filtered_dataset/long_scene/'
+folder_long_weapon = 'filtered_dataset/long_weapon/'
+folder_short_scene = 'filtered_dataset/short_scene/'
+folder_short_weapon = 'filtered_dataset/short_weapon/'
+folder_other = 'filtered_dataset/other/'
 
-    for image_path in images_to_load:
-        image_name = image_path[image_path.rfind('/') + 1: ]
+#print("Folder to load: " + '\n'.join(folders_to_load))
 
-        # show the output image
-        img = cv2.imread(image_path)
-        cv2.imshow("Image", img)
-        key = cv2.waitKey(0)
+#for folder in folders_to_load:
+#    images_to_load = glob.glob(folder + '/*.jpg')
+#    print('Processing: ' + str(folder))
 
-        if (key == ord('q')):   # long_scene
-            cv2.imwrite(folder_long_scene + image_name, img)
-            print("Q")
-        elif (key == ord('w')): # long_weapon
-            cv2.imwrite(folder_long_weapon + image_name, img)
-            print("W")
-        elif (key == ord('e')): # short_scene
-            cv2.imwrite(folder_short_scene + image_name, img)
-            print("E")
-        elif (key == ord('r')): # short_weapon
-            cv2.imwrite(folder_short_weapon + image_name, img)
-            print("R")
-        else:  # other
-            cv2.imwrite(folder_other + image_name, img)
-            print("Other")
+images_to_load = glob.glob(args['images'] + '*.jpg')
+print(images_to_load)
 
-        cv2.destroyAllWindows()
+for image_path in images_to_load:
+    image_name = image_path[image_path.rfind('/') + 1: ]
 
+    # show the output image
+    img = cv2.imread(image_path)
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(0)
+
+    if (key == ord('q')):   # long_scene
+        cv2.imwrite(folder_long_scene + image_name, img)
+        print("Q")
+    elif (key == ord('w')): # long_weapon
+        cv2.imwrite(folder_long_weapon + image_name, img)
+        print("W")
+    elif (key == ord('e')): # short_scene
+        cv2.imwrite(folder_short_scene + image_name, img)
+        print("E")
+    elif (key == ord('r')): # short_weapon
+        cv2.imwrite(folder_short_weapon + image_name, img)
+        print("R")
+    else:  # other
+        cv2.imwrite(folder_other + image_name, img)
+        print("Other")
+
+    cv2.destroyAllWindows()
+
+    '''
     # Remove folder
-    print("Rmoving: " + folder)
+    print("Removing: " + folder)
     shutil.rmtree(folder, ignore_errors=True)
+    '''
