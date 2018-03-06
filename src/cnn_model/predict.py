fcf0ec5b50f061d2709dc6ffd4cfc64e297d2d66
NoName115 <robert.kolcun@gmail.com>
2018-03-06 12:44:28 +0100

    [CNN] predict & train script edited

--- src/cnn_model/predict.py
+++ src/cnn_model/predict.py
@@ -5,13 +5,22 @@ from loader import DataLoader
 
 args = parse_arguments_prediction()
 
-model, image_width, image_height, labels = DataLoader.load_model_data(
+model_data = DataLoader.load_model_data(
     args['model']
 )
+
+model = model_data[0]
+image_width = model_data[1]
+image_height = model_data[2]
+labels = model_data[3]
+preproc = model_data[4]
+datagen = model_data[5]
+
 image = DataLoader.load_and_preprocess_image(
     args['image'],
     image_width,
-    image_height
+    image_height,
+    preproc
 )
 
 result = model.predict(image)[0]
--- src/cnn_model/train.py
+++ src/cnn_model/train.py
@@ -1,10 +1,11 @@
 from keras.optimizers import Adam
-from base import parse_arguments_training
-from loader import DataLoader
+from base import parse_arguments_training, test_training
+from loader import DataLoader, DataSaver
 from models import LeNet, KerasBlog
+from preprocessing import Preprocessor, Preprocessing
 
 
-EPOCHS = 10
+EPOCHS = 1
 INIT_LR = 1e-3
 BS = 16
 IMAGE_WIDTH = 50
@@ -13,18 +14,32 @@ IMAGE_HEIGHT = 50
 # --model, --image
 args = parse_arguments_training()
 
-# Load input images
+# Load input images & split it
 images, labels, labels_dict = DataLoader.load_scaled_data_with_labels(
      'dataset/training_data/',
      IMAGE_WIDTH,
      IMAGE_HEIGHT
 )
-train_x, train_y, test_x, test_y = DataLoader.split_data(images, labels)
-data_gen = DataLoader.get_default_datagen()
+
+splited_data = DataLoader.split_data(images, labels)
+train_x, train_y = splited_data[0:2]
+val_x, val_y = splited_data[2:4]
+test_x, test_y = splited_data[4:6]
+
+# Preprocessing
+prepro = Preprocessor()
+prepro.add_func(Preprocessing.normalize)
+prepro.add_func(Preprocessing.grayscale)
+prepro.add_func(Preprocessing.reshape)
+train_x = prepro.apply(train_x)
+val_x = prepro.apply(val_x)
+
+prepro.set_datagen()
+
 
 # Building model
 #model = LeNet(IMAGE_WIDTH, IMAGE_HEIGHT, labels_dict)
-model = KerasBlog(IMAGE_WIDTH, IMAGE_HEIGHT, labels_dict)
+model = KerasBlog(IMAGE_WIDTH, IMAGE_HEIGHT, labels_dict, depth=1)
 '''
 opt = Adam(
     lr=INIT_LR,
@@ -36,16 +51,20 @@ model.model.compile(
     metrics=['accuracy']
 )
 '''
-model.model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-
-results = model.model.fit_generator(
-    data_gen.flow(train_x, train_y, batch_size=BS),
-    steps_per_epoch=2000 // BS, # steps_per_epoch=len(train_x) // BS,
-    epochs=EPOCHS,
-    validation_data=(test_x, test_y),
-    validation_steps=800 // BS
+model.model.compile(
+    loss='binary_crossentropy',
+    optimizer='rmsprop',
+    metrics=['accuracy']
 )
 
-model.save(args["model"])
+results = model.model.fit_generator(
+    prepro.get_datagen().flow(train_x, train_y, batch_size=BS),
+    steps_per_epoch=len(train_x) // BS,
+    epochs=EPOCHS,
+    validation_data=(val_x, val_y),
+    validation_steps=len(val_x) // BS
+)
+
+#DataSaver.save_model(args["model"], model, prepro)
+
+test_training(test_x, test_y, model.model, labels_dict)
