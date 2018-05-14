4fa60e5200b604152acdb582c1b6c0d10eea8685
NoName115 <robert.kolcun@gmail.com>
2018-05-14 18:19:13 +0200

    [doc] conclusion & results

--- src/models/predict.py
+++ src/models/predict.py
@@ -11,9 +11,9 @@ args = parse_arguments_prediction()
 
 # Load all models
 model_class, prepro_class = DataLoader.load_model_data(args['class'])
-model_angle_x, prepro_x = DataLoader.load_model_data(args['anglex'])
-model_angle_y, prepro_y = DataLoader.load_model_data(args['angley'])
-model_angle_z, prepro_z = DataLoader.load_model_data(args['anglez'])
+model_angle_x, prepro_x = DataLoader.load_model_data(args['anglep'])
+model_angle_y, prepro_y = DataLoader.load_model_data(args['angler'])
+model_angle_z, prepro_z = DataLoader.load_model_data(args['angley'])
 
 # Load & Reshape image
 image = DataLoader.load_image(
@@ -28,7 +28,7 @@ angle_x = get_prediction(model_angle_x, prepro_x.apply(image))
 angle_y = get_prediction(model_angle_y, prepro_y.apply(image))
 angle_z = get_prediction(model_angle_z, prepro_z.apply(image))
 
-print("Type: {0}\tOrientation: {1}-{2}-{3} (x, y, z)".format(
+print("Type: {0}\tOrientation: {1}-{2}-{3} (p, r, y)".format(
     get_max(weapon_type),
     get_max(angle_x), get_max(angle_y), get_max(angle_z)
 ))
